397334ef4f9ca31fef5961554161405014adf9b2
thecursehunter <PhamQuangMinhMDCH@proton.me>
2024-10-24 15:15:12 +0700

    Updated Dashboards and Add Books function

---
+++ LIBDG/LoginForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LIBDG
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtUsername_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (txtUsername.Text == "Username")
+            {
+                txtUsername.Clear();
+            }
+        }
+            
+        private void txtPassword_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (txtPassword.Text == "Password")
+            {
+                txtPassword.Clear();
+                txtPassword.PasswordChar = '*';
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Dashboard dsa = new Dashboard();  
+            dsa.Show();
+        }
+    }
+}
