4aa0a02ae23fd7f31dd41661e5fb0c5195a8898b
CurseHunter <PhamQuangMinhMDCH@proton.me>
2024-10-30 17:03:32 +0700

    Edited "View Book"

--- LIBDG/ViewBook.cs
+++ LIBDG/ViewBook.cs
@@ -1,33 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace LIBDG
 {
     public partial class ViewBook : Form
     {
-        private Library library;
+        private Library library; 
         public ViewBook()
         {
             InitializeComponent();
+            Library library = new Library(); 
         }
+        private void DisplayBooksInDataGridView(List<Book> books)
+        {
+            // Xóa các dòng hiện có trong DataGridView
+            dataGridViewBooks.Rows.Clear();
 
-        private void button1_Click(object sender, EventArgs e)
+            // Thêm từng sách vào DataGridView
+            foreach (Book book in books)
+            {
+                dataGridViewBooks.Rows.Add(book.Title, book.Author, book.ISBN, book.PublishedYear, book.AvailableCopies);
+            } 
+
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
 
-        //Update button
-        private void button3_Click(object sender, EventArgs e)
+        private void btnSearch_Click_Click(object sender, EventArgs e)
         {
-            
+            string searchTerm = txtSearch.Text;
+            string filePath = "book.json";
+            List<Book> foundBooks = Library.LoadBooksFromJsonAndFind(filePath, searchTerm);
+
+            DisplayBooksInDataGridView(foundBooks);
         }
     }
 }
-   
