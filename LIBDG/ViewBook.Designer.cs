2c22e9615d214f4336b18f4d629b6e2cf76cd3aa
Thịnh Trần Phạm Quang <Thinhdragon2307@gmail.com>
2024-11-01 16:40:12 +0700

    add close event for 2 button in AddBook.cs and ViewBook.cs

--- LIBDG/ViewBook.Designer.cs
+++ LIBDG/ViewBook.Designer.cs
@@ -302,6 +302,7 @@
             this.button4.TabIndex = 30;
             this.button4.Text = "Cancel";
             this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // ViewBook
             // 
